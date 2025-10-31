41b38bf1ac8bab74e306e2fec1bde5820eb69e3a
Julito <octaviomosq26@gmail.com>
2025-10-30 22:36:34 -0300

    Backend running on Render (api.ts config updated)

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_BASE_URL = isProduction 
-  ? 'https://tu-backend.onrender.com'  // TO-DO reemplazar luego
+  ? 'https://spotify-clon-zjoy.onrender.com'
   : 'http://localhost:3001';
 
 export const config = {
