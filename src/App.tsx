3bd83d62c2e1cc44a290468d27461649c58feb1d
Julito <octaviomosq26@gmail.com>
2025-10-30 23:11:34 -0300

    Changed to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Search, Library, Playlist } from './pages';
 import { Sidebar, Player } from './components/Layout';
 import LikedSongs from './pages/LikedSongs';
